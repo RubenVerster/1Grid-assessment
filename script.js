d809b532f70658d81618e1f3157dd153d1044819
Ruben Verster <ruben.verster@younglings.africa>
2021-02-08 17:24:46 +0200

    regex for date extraction implemented, restructure of code into IIFE, better res.json() handle

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const fetch = require('node-fetch');
 const args = process.argv;
 
 const email = args[2];
-const userUrl = 'https://whmcstest.proxy.beeceptor.com/client/';
-const invoiceUrl = 'https://whmcstest.proxy.beeceptor.com/invoices/';
+const userUrl = 'https://whmcstest.proxy.beeceptor.com/client';
+const invoiceUrl = 'https://whmcstest.proxy.beeceptor.com/invoices';
 
-const logDetails = ({
+const getRexegDate = (date) => {
+  result = date.match(
+    '[0-9]{2}([-/ .])[0-9]{2}([-/ .])[0-9]{4}([-/ .])[0-9]{2}([-: .])[0-9]{2}'
+  );
+  return result != null ? result[0] : 'Could Not Find Last Login For User';
+};
+
+const logUserDetails = ({
   firstname,
   lastname,
   email,
@@ -16,50 +23,43 @@ const logDetails = ({
   lastlogin,
   currency_code,
 }) => {
-  console.log('==================================================');
-  console.log(`${firstname} ${lastname} (${email})`);
-  console.log(`ID: ${id}`);
-  console.log(`Status: ${status}`);
-  console.log(`Credit: ${currency_code} ${credit}`);
-  console.log(`Last Login: ${lastlogin.substr(6, 10)}`);
+  console.log(
+    `==================================================  
+    ${firstname} ${lastname} (${email}) 
+    ID: ${id} 
+    Status: ${status} 
+    Credit: ${currency_code} ${credit} 
+    Last Login: ${getRexegDate(lastlogin)}`
+  );
 };
 
-const logInvoices = async ({ id }) => {
-  try {
-    const response = await fetch(`${invoiceUrl}${id}`);
-    const userInvoicesResults = await response.json();
-    const userInvoices = userInvoicesResults.invoices.invoice;
+const logUserInvoices = (invoicesArr = []) => {
+  console.log(`\n Orders \n # - InvoiceId - Status - Date - Due_Date - Total`);
 
-    console.log('\n', 'Orders');
-    console.log('# - InvoiceId - Status - Date - Due_Date - Total');
-    userInvoices.forEach(
-      ({ id, status, date, duedate, total, currencycode }, index) =>
-        console.log(
-          `${
-            index + 1
-          }. ${id} | ${status} | ${date} | ${duedate} | ${currencycode} ${total}`
-        )
+  invoicesArr.forEach(
+    ({ id, status, date, duedate, total, currencycode }, index) =>
+      console.log(
+        `${
+          index + 1
+        }. ${id} | ${status} | ${date} | ${duedate} | ${currencycode} ${total}`
+      )
+  );
+};
+
+(async function (email) {
+  try {
+    const userProfile = await fetch(`${userUrl}/${email}`).then((response) =>
+      response.json()
     );
-  } catch {
-    console.error(`Failed to retrieve invoices for user with id: ${id}`);
-    console.log('See below for more details on the error:');
-    console.error(err);
-  }
-};
+    const userInvoices = await fetch(
+      `${invoiceUrl}/${userProfile.id}`
+    ).then((response) => response.json());
 
-fetchUserDetails = async (userUrl, email) => {
-  try {
-    const response = await fetch(`${userUrl}${email}`);
-    const userProfile = await response.json();
-
-    logDetails(userProfile);
-    logInvoices(userProfile);
-  } catch (err) {
-    // handle error
+    logUserDetails(userProfile);
+    logUserInvoices(userInvoices.invoices.invoice);
+  } catch (error) {
     console.error(`User with email: ${email} cannot be found`);
     console.log('See below for more details on the error:');
-    console.error(err);
+    console.error(error);
   }
-};
-
-fetchUserDetails(userUrl, email);
+})(email);
